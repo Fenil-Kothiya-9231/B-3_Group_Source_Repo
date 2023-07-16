2573b72bf0499ec1483f1d68b01af41ce44feba4
**** <****>
2023-07-16 15:51:38 +0530

    All C-shape Problem Fixed Version Updateted & Working Proparlly --v.1.1. fe

--- C_shape/src/App.js
+++ C_shape/src/App.js
@@ -1,26 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const Box = ({ onClick, isClicked }) => {
   const boxColor = isClicked ? 'green' : 'white';
   return (
-    <div className={`box ${boxColor}`} onClick={onClick}>
-    </div>
+    <div className={`box ${boxColor}`} onClick={onClick}></div>
   );
 };
 
 const App = () => {
   const [boxStates, setBoxStates] = useState(Array(7));
+  const [ClickedIndexArr, setClickedIndexArr] = useState([]);
 
   const handleClick = (index) => {
     const updatedBoxStates = [...boxStates];
     updatedBoxStates[index] = !updatedBoxStates[index];
     setBoxStates(updatedBoxStates);
+
+    setClickedIndexArr(prevArr => [...prevArr, index]);
   };
+
+  useEffect(() => {
+    if (ClickedIndexArr.length > 0) {
+      const timer = setTimeout(() => {
+        const lastClickedIndex = ClickedIndexArr[ClickedIndexArr.length - 1];
+        const updatedBoxStates = [...boxStates];
+        updatedBoxStates[lastClickedIndex] = false;
+        setBoxStates(updatedBoxStates);
+
+        setClickedIndexArr(prevArr => prevArr.slice(0, -1));
+      }, 1000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [ClickedIndexArr, boxStates]);
 
   return (
     <div className='appdiv'>
-      <h1>C-Shape Problem Version(v.1.0.fe)</h1>
+      <h1>C-Shape Problem ( Version v.1.0.fe )</h1>
       <div className="row">
         <Box onClick={() => handleClick(0)} isClicked={boxStates[0]} />
         <Box onClick={() => handleClick(1)} isClicked={boxStates[1]} />
@@ -34,6 +51,7 @@ const App = () => {
         <Box onClick={() => handleClick(5)} isClicked={boxStates[5]} />
         <Box onClick={() => handleClick(6)} isClicked={boxStates[6]} />
       </div>
+      <p>Clicked Indices: {ClickedIndexArr.join(', ')}</p>
     </div>
   );
 };
