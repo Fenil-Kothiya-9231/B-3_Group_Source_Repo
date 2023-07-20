3dfa8d8dacf1996f804ab29d6d89816e3dce4573
**** <****>
2023-07-20 12:02:23 +0530

    in the movie api project we have 10 data under movie_api_10.js file ...v.1.0.0

---
+++ movie_api/movies_api_10.js
@@ -0,0 +1,77 @@
+
+  let movies = [
+    {
+      "moviename": "Dilwale Dulhania Le Jayenge",
+      "leadactor": "Shah Rukh Khan",
+      "directors": ["Aditya Chopra"],
+      "castnames": ["Kajol", "Amrish Puri", "Anupam Kher"],
+      "cameraassistants": ["Ravi K. Chandran", "K. U. Mohanan"]
+    },
+    {
+      "moviename": "3 Idiots",
+      "leadactor": "Aamir Khan",
+      "directors": ["Rajkumar Hirani"],
+      "castnames": ["Kareena Kapoor Khan", "R. Madhavan", "Sharman Joshi"],
+      "cameraassistants": ["C. K. Muraleedharan", "Murali G."]
+    },
+    {
+      "moviename": "Kabhi Khushi Kabhie Gham",
+      "leadactor": "Shah Rukh Khan",
+      "directors": ["Karan Johar"],
+      "castnames": ["Kajol", "Hrithik Roshan", "Amitabh Bachchan"],
+      "cameraassistants": ["Manmohan Singh", "Ravi K. Chandran"]
+    },
+    {
+      "moviename": "Bajrangi Bhaijaan",
+      "leadactor": "Salman Khan",
+      "directors": ["Kabir Khan"],
+      "castnames": ["Kareena Kapoor Khan", "Nawazuddin Siddiqui", "Harshaali Malhotra"],
+      "cameraassistants": ["Aseem Mishra", "Anay Goswami"]
+    },
+    {
+      "moviename": "Gangs of Wasseypur",
+      "leadactor": "Manoj Bajpayee",
+      "directors": ["Anurag Kashyap"],
+      "castnames": ["Nawazuddin Siddiqui", "Richa Chadda", "Huma Qureshi"],
+      "cameraassistants": ["Rajeev Ravi", "Pankaj Kumar"]
+    },
+    {
+      "moviename": "Dil Chahta Hai",
+      "leadactor": "Aamir Khan",
+      "directors": ["Farhan Akhtar"],
+      "castnames": ["Saif Ali Khan", "Akshaye Khanna", "Preity Zinta"],
+      "cameraassistants": ["Ravi K. Chandran", "Kartik Vijay"]
+    },
+    {
+      "moviename": "Kuch Kuch Hota Hai",
+      "leadactor": "Shah Rukh Khan",
+      "directors": ["Karan Johar"],
+      "castnames": ["Kajol", "Rani Mukerji", "Salman Khan"],
+      "cameraassistants": ["Santosh Thundiyil", "Ravi K. Chandran"]
+    },
+    {
+      "moviename": "Padmaavat",
+      "leadactor": "Deepika Padukone",
+      "directors": ["Sanjay Leela Bhansali"],
+      "castnames": ["Shahid Kapoor", "Ranveer Singh", "Aditi Rao Hydari"],
+      "cameraassistants": ["Sudeep Chatterjee", "Hari Vedantam"]
+    },
+    {
+      "moviename": "Koi... Mil Gaya",
+      "leadactor": "Hrithik Roshan",
+      "directors": ["Rakesh Roshan"],
+      "castnames": ["Preity Zinta", "Rekha", "Rajat Bedi"],
+      "cameraassistants": ["Sameer Arya", "Ravi K. Chandran"]
+    },
+    {
+      "moviename": "Kal Ho Naa Ho",
+      "leadactor": "Shah Rukh Khan",
+      "directors": ["Nikkhil Advani"],
+      "castnames": ["Preity Zinta", "Saif Ali Khan", "Jaya Bachchan"],
+      "cameraassistants": ["Anil Mehta", "Ravi K. Chandran"]
+    }
+  ]
+
+
+
+export default movies ;
